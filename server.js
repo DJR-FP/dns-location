732eda6f6e642b2b47bbe23179ad049c4ad4cc86
donald <donald@fullproxy.com>
2023-02-22 16:26:30 +0000

    error in dns fixed

--- server.js
+++ server.js
@@ -34,20 +34,21 @@ app.get('/', async (req, res) => {
   res.write(`<p>Using DNS server: ${dnsServerIp}</p>`);
 
   try {
-    const results = await Promise.all(
-      dnsNames.map((name) => {
-        return new Promise((resolve, reject) => {
-          dns.lookup(name, (err, address, family) => {
-            if (err) {
-              reject(err);
-            } else {
-              const location = ipToLocationMap[address] || 'location unknown';
-              resolve({ name, address, location });
-            }
-          });
-        });
-      })
-    );
+   const results = await Promise.all(
+  dnsNames.map((name) => {
+    return new Promise((resolve) => {
+      dns.lookup(name, { all: true, family: 4, server: dnsServerIp }, (err, addresses) => {
+        if (err) {
+          console.log(`Error querying ${name}: ${err}`);
+          resolve({ name, address: '0.0.0.0', location: 'location unknown' });
+        } else {
+          const location = ipToLocationMap[addresses[0].address] || 'location unknown';
+          resolve({ name, address: addresses[0].address, location });
+        }
+      });
+    });
+  })
+);
 
     results.forEach(({ name, address, location }) => {
       res.write(`<p>${name} resolves to ${address} (${location})</p>`);
@@ -67,16 +68,23 @@ setInterval(() => {
   console.log(`Querying DNS names: ${dnsNames}`);
   dnsNames.forEach((name) => {
     dns.lookup(name, { all: true, family: 4, server: dnsServerIp }, (err, addresses) => {
-  if (err) {
-    console.log(`Error querying ${name}: ${err}`);
-  } else {
-    const location = ipToLocationMap[addresses[0].address] || 'location unknown';
-    const logMessage = `${new Date().toISOString()} - ${name} resolves to ${addresses[0].address} (${location})\n`;
-    console.log(logMessage);
-    fs.appendFileSync(logFilePath, logMessage);
-  }
-});
-
+      try {
+        if (err) throw err;
+        const location = ipToLocationMap[addresses[0].address] || 'location unknown';
+        const logMessage = `${new Date().toISOString()} - ${name} resolves to ${addresses[0].address} (${location})\n`;
+        console.log(logMessage);
+        fs.appendFileSync(logFilePath, logMessage);
+      } catch (err) {
+        const logMessage = `${new Date().toISOString()} - Error querying ${name}: ${err}\n`;
+        console.log(logMessage);
+        fs.appendFileSync(logFilePath, logMessage);
+        const address = '0.0.0.0';
+        const location = ipToLocationMap[address] || 'location unknown';
+        const defaultLogMessage = `${new Date().toISOString()} - ${name} resolves to ${address} (${location})\n`;
+        console.log(defaultLogMessage);
+        fs.appendFileSync(logFilePath, defaultLogMessage);
+      }
+    });
   });
 }, 30000);
 
